4fb97824a620b47c672a5fb95e5ebfcb64883b5b
Quintin Vomocil <quintinvomocil@gmail.com>
2022-07-01 10:26:43 -0600

    Finished to the profile section.

--- about.js
+++ about.js
@@ -5,10 +5,18 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Form Was Submitted Successfully');
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+const duckPhoto = document.querySelector('img')
+
+const compliment = () => {
+	alert("You look even better than this duck!")
+ }
+
+ duckPhoto.addEventListener('mouseover', compliment)
